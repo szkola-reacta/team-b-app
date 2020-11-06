b897a66010266d397511d4b0a88bc8fe9f4076dd
Karol <guzik912@o2.pl>
2020-11-06 12:58:01 +0100

    implenet videoList component into App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
-import "./App.css";
+import './App.css';
+import { VideoList } from './components/VideoList';
 
 function App() {
-  return <div className="App">test - feel free to remove</div>;
+  return (
+    <div className='App'>
+      <VideoList />
+    </div>
+  );
 }
 
 export default App;
